1f40f710370c370c4310507e06d82a35c9df27cd
Do Ngoc Son <sondo1@cqtdt.asia>
2025-04-21 14:09:26 +0700

    code base

--- SingpassClient/src/apis/auth.api.js
+++
@@ -1,8 +0,0 @@
-import api from "./httpClient";
-
-export const loginAPI = async (data) => {
-  return await api.post("/auth/local", {
-    identifier: data.email,
-    password: data.password,
-  });
-};
